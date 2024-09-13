f20713395fcd910ef3311f08474aeb6c428abb97
zhshly007 <zhshly007@163.com>
2024-09-14 06:15:15 +0800

    add BerryCommon of class

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BerryCommon {
+
+    public:
+        int m_position;
+    
+    
+
+};
+
+
+int main(int argc,char** args) {
+
+
+    return 0;
+}
